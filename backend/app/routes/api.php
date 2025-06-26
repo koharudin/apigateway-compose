d8bd44d3804432e681e93adec086be1c45609d37
Koharudin <koha001>
2025-06-26 10:41:08 +0700

    backend apigateway

--- backend/app/routes/api.php
+++ backend/app/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json(["message"=>"backend Laravel"]);
+    return response()->json(["message"=>"backend Laravel ".date("Y-m-d H:i:s")]);
 });
 
 Route::get('/endpoint1', function () {
